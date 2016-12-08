7274c591880473f8cf0a35460d3678f40bac2505
johnhu <huzichunjohn@126.com>
2016-12-08 19:44:31 +0800

    use spider arguments.

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,16 +2,19 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
+
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text') .extract_first(),
                 'author': quote.css('span small::text').extract_first(),
-                'tags': quote.css('div.tags a.tag::text').extract(),
             }
 
         next_page = response.css('li.next a::attr(href)').extract_first()
