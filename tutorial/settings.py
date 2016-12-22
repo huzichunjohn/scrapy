f3aadd04359898660fb3f2afcd27a0e4ef9ef235
johnhu <huzichunjohn@126.com>
2016-12-22 23:43:42 +0800

    fix indent.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -17,9 +17,10 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
+USER_AGENT = 'User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- tutorial/spiders/zhihu_spider.py
+++ tutorial/spiders/zhihu_spider.py
@@ -6,38 +6,47 @@ class ZhihuSpider(scrapy.Spider):
     name = 'zhihu'
     allowed_domains = ['zhihu.com']
     start_urls = [
-	'https://www.zhihu.com/',
+        'https://www.zhihu.com',
     ]
 
     def get_cookies(self):
-	driver = webdriver.Chrome()
-	driver.get(self.start_urls[0])
-	driver.find_element_by_link_text(u'登录').click()
-	driver.find_element_by_name('account').clear()
-	driver.find_element_by_name('account').send_keys("username")
-	dirver.find_element_by_name('password').clear()
-	dirvier.find_element_by_name('password').send_keys("password")
-	SignInURL = u'https://www.zhihu.com/#signin'
+        driver = webdriver.Chrome()
+        driver.get(self.start_urls[0])
+        driver.find_element_by_link_text(u'登录').click()
+        driver.find_element_by_name('account').clear()
+        driver.find_element_by_name('account').send_keys("18600682316")
+        driver.find_element_by_name('password').clear()
+        driver.find_element_by_name('password').send_keys("huzichunJOHN1234")
+        SignInURL = u'https://www.zhihu.com/#signin'
 
-	try:
-	    if dirver.find_element_by_id('captcha'):
-		while True:
-		    if driver.current_url != SignInURL:
-			break
-	finally:
-	    if driver.current_url == SignInURL:
-		driver.find_element_by_css_selector('button.sign-button.submit').click()
-	    cookies = dirver.get_cookies()
-	    dirver.close()
-	    return cookies
+        try:
+            #TODO: add captcha recongnize
+            if dirver.find_element_by_id('captcha'):
+                print "captcha"
+                while True:
+                    print "#" * 20
+                    print driver.current_url
+                    print "#" * 20
+                    if driver.current_url != SignInURL:
+                        break
+        finally:
+            if driver.current_url == SignInURL:
+                driver.find_element_by_css_selector('button.sign-button.submit').click()
+            cookies = driver.get_cookies()
+            #driver.close()
+            import time
+            time.sleep(5)
+            return cookies
 
     def get_titles(self, response):
-	sel = scrapy.Selector(response)
-	for i in range(1, 10):
-	    xml = r'//*[@id="feed-%d"]/div[1]/div[2]/div[2/h2/a/text()]' % (i)
-	    title = sel.xpath(xml).extract()
-	    if len(title) > 0:
-		print str(title[0])	
-	
+        sel = scrapy.Selector(response)
+        for i in range(10):
+            xml = r'//*[@id="feed-%d"]/div[1]/div[2]/div[2]/h2/a/text()' % (i)
+            title = sel.xpath(xml).extract()
+            if len(title) > 0:
+                print "#" * 20
+                print str(title[0])
+                print "#" * 20
+
     def parse(self, response):
-	return scrapy.Request(url=self.start_urls[0], cookies=self.get_cookies(), callback=self.get_titles)
+        return scrapy.Request(url=self.start_urls[0], cookies=self.get_cookies(), callback=self.get_titles)
